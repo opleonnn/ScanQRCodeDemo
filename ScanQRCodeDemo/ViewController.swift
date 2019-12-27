eaef54a25d0cf2ab047a3d2dc7cf9e40dc581ca4
opleonnn <onepieceleonnn@gmail.com>
2019-12-27 11:33:48 +0800

    create project

---
+++ ScanQRCodeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanQRCodeDemo
+//
+//  Created by Leon on 12/27/19.
+//  Copyright © 2019 Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
